412ef3b070e53ba17be08e2eb3a362fa1ec00bf1
Teodora Ivanova <81617194+Teodora217@users.noreply.github.com>
2023-12-17 13:48:12 +0200

    Add GameCreate Add GameList Add Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./component/header/Header";
 import Home from "./component/home/Home";
-import GameList from "./component/GameList/GameList";
+import GameList from "./component/game-list/GameList";
+import GameCreate from "./component/game-create/GameCreate";
+import Login from "./component/login/Login";
 function App() {
   return (
     <div id="box">
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
